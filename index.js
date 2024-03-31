93df381069d3140d2b6ce67de2590b15c9469c6c
Wajiha Ghazanfar <160530110+WajihaGhazanfar@users.noreply.github.com>
2024-04-01 01:21:13 +0500

    Adding a cli-nu,ber guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let RandomNumber = (Math.floor(Math.random() * 10 + 1));
+const GuessedNumber = await inquirer.prompt([
+    {
+        message: "Guess the number between 1-10",
+        type: "number",
+        name: "numbergame",
+    }
+]);
+if (GuessedNumber.numbergame === RandomNumber) {
+    console.log("Congratulations! you guessed the right number");
+}
+else {
+    console.log("You guessed the wrong number");
+}
